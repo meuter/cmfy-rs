e3ff7d13dea0e62873f4c4fd642e229ce988f012
Cédric Meuter <cedric.meuter@gmail.com>
2024-10-19 07:24:54 +0200

    feat: added extra information to the stats command output

--- src/cli/commands/stats.rs
+++ src/cli/commands/stats.rs
@@ -1,32 +1,43 @@
 use super::Run;
 use clap::Args;
 use cmfy::{Client, Result};
+use colored::Colorize;
 use humansize::{make_format, BINARY};
 
-/// Displays basic statistics about the server.
+/// Displays basic statistics about client and server.
 #[derive(Debug, Args)]
 pub struct Stats;
 
 impl Run for Stats {
     async fn run(self, client: Client) -> Result<()> {
+        println!("{}", "client".yellow());
+        println!("    name        : {}", env!("CARGO_PKG_NAME"));
+        println!("    version     : {}", env!("CARGO_PKG_VERSION"));
+        println!("    client_id   : {}", client.id);
+
         let stats = client.system_stats().await?;
-        println!("versions:");
+        println!("{}", "server".yellow());
+        println!("    url         : {}", client.base_url()?);
+        println!("    os          : {}", stats.system.os);
+        println!("    versions");
         println!(
-            "    python  : {}",
+            "        python  : {}{}",
             stats
                 .system
                 .python_version
                 .split_whitespace()
                 .next()
-                .expect("malfored python version")
+                .expect("malfored python version"),
+            if stats.system.embedded_python { " (embedded)" } else { "" }
         );
-        println!("    comfyui : {}", stats.system.comfyui_version);
-        println!("    pytorch : {}", stats.system.pytorch_version);
-        println!("devices:");
+        println!("        comfyui : {}", stats.system.comfyui_version);
+        println!("        pytorch : {}", stats.system.pytorch_version);
+        println!("    devices");
         let format_size = make_format(BINARY);
-        for device in &stats.devices {
+        for (index, device) in stats.devices.iter().enumerate() {
             println!(
-                "    {} ({}/{})",
+                "        {:<8}: {} ({}/{})",
+                format!("[{}]", index),
                 device.name,
                 format_size(device.vram_free),
                 format_size(device.vram_total),
