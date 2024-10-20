0e3448ce26150a8eea50fb87ace0eb149f843ba2
Cédric Meuter <cedric.meuter@gmail.com>
2024-10-20 13:53:28 +0200

    fix(nodes): fixed common definition

--- nodes/src/lib.rs
+++ nodes/src/lib.rs
@@ -7,14 +7,24 @@ pub struct LoraLoaderInputs {
     pub lora_name: String,
     pub strength_clip: f32,
     pub strength_model: f32,
+    #[serde(flatten)]
+    #[node_input(skip)]
+    pub other: serde_json::Value,
 }
 
 
 #[derive(Debug, Clone, Serialize, Deserialize, Node)]
 #[node(class_type = "KSampler")]
 pub struct KSamplerInputs {
-    steps: u8,
-    seed: u64
+    pub cfg: f32,
+    pub denoise: f32,
+    pub sampler_name: String,
+    pub scheduler: String,
+    pub steps: u8,
+    pub seed: u64,
+    #[serde(flatten)]
+    #[node_input(skip)]
+    pub other: serde_json::Value,
 }
 
 
@@ -24,5 +34,12 @@ pub struct EmptyLatentImageInputs {
     pub batch_size: u8,
     pub height: usize,
     pub width: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Node)]
+#[node(class_type = "CLIPTextEncode")]
+pub struct ClipTextEncodeInput {
+    pub text: String,
+    pub clip: serde_json::Value,
 }
 
