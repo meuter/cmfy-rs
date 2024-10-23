3246f88bcd1a756ce1faf93261f770f4f9d40860
Cédric Meuter <cedric.meuter@gmail.com>
2024-10-23 11:06:16 +0200

    feat: removed the possibility to spoof CLIENT_ID

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,6 @@ struct Cli {
     #[arg(short, long, env = "COMFY_PORT", value_name = "PORT", default_value_t = 8188)]
     port: u32,
 
-    /// client id advertised to the server, allows to spoof an existing client
-    /// when listening to websocket message. If none is provided, a unique id
-    /// will be computed.
-    #[arg(short, long, env = "COMFY_CLIENT_ID", value_name = "CLIENT_ID")]
-    client_id: Option<String>,
-
     /// command to execute
     #[command(subcommand)]
     command: Command,
@@ -84,7 +78,7 @@ fn compute_own_client_id() -> String {
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Cli::parse();
-    let client_id = args.client_id.unwrap_or_else(compute_own_client_id);
+    let client_id = compute_own_client_id();
     let client = Client::new(args.hostname, args.port, client_id);
     args.command.run(client).await
 }
